aff2a98ac13e9ac9086b5df6961d022f246931b9
Hafsa Maqsood <maqsoodhafsa28@gmail.com>
2026-07-05 13:00:05 +0500

    Time: 5 ms (79.86%), Space: 45.5 MB (96.76%) - LeetHub

---
+++ 0009-palindrome-number/0009-palindrome-number.java
@@ -0,0 +1,13 @@
+class Solution {
+    public boolean isPalindrome(int x) {
+        int temp=x;
+        int digit=0;
+        int rev=0;
+        while(temp>0){
+            digit=temp%10;
+            rev=(rev*10)+digit;
+            temp=temp/10;
+        }
+        return rev==x;
+    }
+}
