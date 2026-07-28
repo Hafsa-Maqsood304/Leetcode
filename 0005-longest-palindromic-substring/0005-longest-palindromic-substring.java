3c1192b50966fdb3a4d6dd19d705bd42d6f09c9d
Hafsa Maqsood <maqsoodhafsa28@gmail.com>
2026-07-28 22:32:05 +0500

    Time: 22 ms (33.15%), Space: 46.8 MB (31.7%) - LeetHub

---
+++ 0005-longest-palindromic-substring/0005-longest-palindromic-substring.java
@@ -0,0 +1,44 @@
+class Solution {
+
+    public String longestPalindrome(String s) {
+
+        if (s.length() <= 1)
+            return s;
+
+        String ans = "";
+
+        for (int i = 0; i < s.length(); i++) {
+
+            int left = i;
+            int right = i;
+
+            while (left >= 0 && right < s.length()
+                    && s.charAt(left) == s.charAt(right)) {
+
+                if (right - left + 1 > ans.length()) {
+                    ans = s.substring(left, right + 1);
+                }
+
+                left--;
+                right++;
+            }
+
+            // Even length palindrome
+            left = i;
+            right = i + 1;
+
+            while (left >= 0 && right < s.length()
+                    && s.charAt(left) == s.charAt(right)) {
+
+                if (right - left + 1 > ans.length()) {
+                    ans = s.substring(left, right + 1);
+                }
+
+                left--;
+                right++;
+            }
+        }
+
+        return ans;
+    }
+}
