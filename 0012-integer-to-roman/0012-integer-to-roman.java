72ddf2a603e96c3337f62f4e79813d260074c4dd
Hafsa Maqsood <maqsoodhafsa28@gmail.com>
2026-07-29 22:00:16 +0500

    Time: 6 ms (14.63%), Space: 48.8 MB (5.26%) - LeetHub

---
+++ 0012-integer-to-roman/0012-integer-to-roman.java
@@ -0,0 +1,67 @@
+class Solution {
+    public String intToRoman(int num) {
+
+        String value="";
+        while(num>0){
+         if(num>=1000){
+            value+= "M";
+            num-=1000;
+         }
+         else if(num>=900){
+            value+="CM";
+            num-=900;
+         }
+         else if(num>=500){
+            value+="D";
+            num-=500;
+         }
+         
+         else if(num>=400){
+            value+="CD";
+            num-=400;
+         }
+         else if(num>=100){
+            value+="C";
+            num-=100;
+         }
+         
+         else if(num>=90){
+            value+="XC";
+            num-=90;
+         }
+         else if(num>=50){
+            value+="L";
+            num-=50;
+         }
+         
+         else if(num>=40){
+            value+="XL";
+            num-=40;
+         }
+         else if(num>=10){
+            value+="X";
+            num-=10;
+         }
+         
+         else if(num>=9){
+            value+="IX";
+            num-=9;
+         }
+         else if(num>=5){
+            value+="V";
+            num-=5;
+         }
+         
+         else if(num>=4){
+            value+="IV";
+            num-=4;
+         }
+         else {
+         value+="I";
+         num--;
+         }
+        }
+         return value;
+   
+    }
+}
