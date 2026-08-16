fa9e37de5c28bc984eb28bc895562a92400a4cdf
Hafsa Maqsood <maqsoodhafsa28@gmail.com>
2026-08-16 11:11:24 +0500

    Time: 0 ms (100%), Space: 47.9 MB (57.82%) - LeetHub

---
+++ 0050-powx-n/0050-powx-n.java
@@ -0,0 +1,25 @@
+class Solution {
+    public double myPow(double x, int n) {
+
+        long num = n;
+
+        if (num < 0) {
+            x = 1 / x;
+            num = -num;
+        }
+
+        double result = 1;
+
+        while (num > 0) {
+
+            if (num % 2 == 1) {
+                result *= x;
+            }
+
+            x *= x;
+            num /= 2;
+        }
+
+        return result;
+    }
+}
